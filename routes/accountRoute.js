14f95bd06d1b26cc319842208a046a49546e3126
Zach Barnett <zacharybarnett96@gmail.com>
2025-11-22 21:27:58 -0800

    Input validation

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,11 +3,18 @@ const express = require('express');
 const router = new express.Router();
 const accountController = require('../controllers/accountController');
 const utilities = require('../utilities/');
+const regValidate = require('../utilities/account-validation');
 
 // Route to build account login page
 router.get('/login', utilities.handleErrors(accountController.buildLogin));
 // Route to build registration page
 router.get('/register', utilities.handleErrors(accountController.buildRegister));
 // Route to receive registration form POST
-router.post('/register', utilities.handleErrors(accountController.registerAccount));
+router.post(
+	'/register',
+	regValidate.registrationRules(),
+	regValidate.checkReqData,
+	utilities.handleErrors(accountController.registerAccount)
+);
+
 module.exports = router;
